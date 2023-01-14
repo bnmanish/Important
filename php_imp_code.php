cd1e61014eb040a3b83a9e016af6ab5f899f21a1
B N Manish <89665665+bnmanish@users.noreply.github.com>
2023-01-14 15:08:41 +0530

    Update php_imp_code.php

--- php_imp_code.php
+++ php_imp_code.php
@@ -1,3 +1,31 @@
 //use to convert string special charecter in html intity
 htmlspecialchars($str, ENT_QUOTES)
 //use to convert string special charecter in html intity
+
+//-----------------------------------------------------
+.package-content ul li:before {
+    content: '✔ ';
+    margin-left: -20px;
+    color: #f05d45;
+}
+
+//-----------------------------------------------------
+n level dropdown
+---------------------
+function nlevelcat($parent_id,$level = Null, $prefix = '') {
+    $catdata = array();
+    $data = ProductCategory::orderBy('title')->where(['parent_id'=>$level,'status'=>1])->get();
+    $category = '';
+    if (count($data) > 0) {
+        foreach ($data as $row) {
+            if($parent_id == $row->id){
+                $check = "selected";
+            }else{
+                $check = "";
+            }
+            $category .= "<option value='$row->id' ".$check.">".$prefix . $row->title."</option>";
+            $category .= nlevelcat($parent_id,$row->id, $prefix . ' - ');
+        }
+    }
+    return $category;
+}
